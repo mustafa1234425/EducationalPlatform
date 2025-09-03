39d6e9330a1ffd0157a72512f4b166d860f82920
Mustafa Mohamed <mostafammmm985@gmail.com>
2025-09-03 16:24:10 +0300

    ci: add GitHub Actions workflow for build and test
    - Added .GitHub/workflows/ci-cd.yml
    - Workflow triggers on pull request and push to feature/ and bugfix/ branches
    - Runs restore, build, and test steps

--- EducationalPlatform.Application/obj/Debug/net9.0/EducationalPlatform.Application.AssemblyInfo.cs
+++ EducationalPlatform.Application/obj/Debug/net9.0/EducationalPlatform.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EducationalPlatform.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc139c64b6af63ed5980552ef1549845d6fc40d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e5a8858b12daf51b658576ed1148b4619543c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EducationalPlatform.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EducationalPlatform.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
